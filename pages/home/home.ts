de19560e4db803776ba78d18d8c6cfd0555bcf2a
TrashHat <julian.dolojan160340@my.lsgh.edu.ph>
2021-01-21 21:07:28 +0800

    3 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { EmbedVideoService } from 'ngx-embed-video';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+yt_iframe_html: any; 
 
-  constructor(public navCtrl: NavController) {
+youtubeUrl = "https://www.youtube.com/watch?v=VEQaH4LruUo";
 
-  }
-
+ constructor(
+    private embedService: EmbedVideoService
+  ) {
+    this.yt_iframe_html = this.embedService.embed(this.youtubeUrl);
+}
 }
